d4d1d12f2b5d65c626f8c9fc8e7c4f8a543fa136
Boltztt66 <136881441+Boltztt66@users.noreply.github.com>
2023-07-29 21:13:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 11264403
+API_HASH     = "c141f83588c377f60dfcf5512d19519a"
+BOT_TOKEN    = "6501196483:AAGmB-ZWP6frLj3ha73uJCR5qnDK19ScMZs"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+DATABASE_URI = "mongodb+srv://Rider9:2442339024@cluster0.ini1poa.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001950559128
+ADMIN        = 5895738935
+CHANNEL      = "@log2442339024"
