acd9ed2706804c06618f3176cc62917d7ae5b194
Boltztt66 <136881441+Boltztt66@users.noreply.github.com>
2023-07-29 21:28:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 11264403
 API_HASH     = "c141f83588c377f60dfcf5512d19519a"
-BOT_TOKEN    = "6501196483:AAGmB-ZWP6frLj3ha73uJCR5qnDK19ScMZs"
+BOT_TOKEN    = "6651903613:AAGNQnCfNzuJQHfWnSBXDQ3t4XPt9ibdR6U"
 SESSION      = ""
 DATABASE_URI = "mongodb+srv://Rider9:2442339024@cluster0.ini1poa.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001950559128
