e76a4cbe6811fa151c3dadcbac4fcdc0b00279aa
Boltztt66 <136881441+Boltztt66@users.noreply.github.com>
2023-08-01 16:37:33 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 11264403
 API_HASH     = "c141f83588c377f60dfcf5512d19519a"
 BOT_TOKEN    = "6651903613:AAGNQnCfNzuJQHfWnSBXDQ3t4XPt9ibdR6U"
 SESSION      = "BQCr4ZMAks36WcnWCCOnNIVqBm8CciXMHtWzMHrMbU7hg-BlBkDU41psRm7oG-xBnWhbNPkiaoMn_J6HQ62kh1Ne_Tn6NSFDrJGUfvS8vfB6NNpvK0VM-HQksfJY6Be7LyORaoaQYdvbM-MYZRksWBa5jJPyu0CuD2nrT6aczciHg8rkasvmoKjQkvQsOJDlf3xbMs3GnW3_DSZUdI9aY7vMdvchDXrsOXCm6Vlc_vpZAbOGlarJxtME-K38AfY378zOQsiyYJKyyqsnWypUKLCOmR2gjyYNZsZSpRy0ccMNyYLzHobqcJsbKQvt9nSKW2gfJ9x2tiQAbC_1FqqbaT1ii6VOKQAAAAF6AHa-AA"
-DATABASE_URI = "mongodb+srv://Rider9:2442339024@cluster0.ini1poa.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Golamj:24423390@cluster0.2b0pdpa.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001950559128
 ADMIN        = 6341818046
 CHANNEL      = "@log2442339024"
