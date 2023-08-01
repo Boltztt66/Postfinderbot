6e1e42036cf9810d39f3a668775329cd293d5bfb
Boltztt66 <136881441+Boltztt66@users.noreply.github.com>
2023-08-01 11:05:22 +0530

    Update search.py

--- plugins/search.py
+++ plugins/search.py
@@ -33,7 +33,7 @@ async def search(bot, message):
               buttons.append([InlineKeyboardButton(movie['title'], callback_data=f"recheck_{movie['id']}")])
           msg = await message.reply_photo(photo="",
                                           caption="<b><I>I Couldn't find anything related to Your Query😕.\nDid you mean any of these?</I></b>", 
-                                          
+                                          reply_markup=InlineKeyboardMarkup(buttons))
        else:
           msg = await message.reply_text(text=head+results, disable_web_page_preview=True)
        _time = (int(time()) + (15*60))
